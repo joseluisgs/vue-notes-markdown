102ba624c940930c68f07f7bcc691b2c2925b412
José Luis González <joseluisgonsan@gmail.com>
2022-07-29 10:09:24 +0200

    Maquetado inicial

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup></script>
 
 <template>
-  <p>Hola!</p>
+  <div class="dev | container | p-3 my-3 mx-auto | flex">
+    <!-- parte izquierda -->
+    <section class="dev | p-3 mr-3">
+      <!-- busquedas de notas -->
+      <p>Search Notes</p>
+      <!-- Lista de notas -->
+      <p>Note List</p>
+    </section>
+    <!-- parte derecha flex grow para que crezca todo-->
+    <section class="dev | p-3 flex-grow">
+      <!-- nota activa -->
+      <p>Active Note</p>
+    </section>
+  </div>
 </template>
 
 <style scoped>
 /** Mis estilos */
+.dev {
+  border: 1px solid red;
+}
 </style>
