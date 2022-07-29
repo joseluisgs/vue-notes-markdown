1014b6d5b00db848fe99aff912ffabccf5d71e6e
José Luis González <joseluisgonsan@gmail.com>
2022-07-29 09:06:40 +0200

    Configuracion de Estilos

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,6 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ]
+  root: true,
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
 }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'virtual:windi.css'
+
 import './assets/main.css'
 
 const app = createApp(App)
