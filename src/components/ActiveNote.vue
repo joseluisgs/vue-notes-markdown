f3ef45166655818bd80a2f26ceadc89fee6f1083
José Luis González <joseluisgonsan@gmail.com>
2022-07-29 13:40:46 +0200

    Extrapolar componente a HTML

--- src/components/ActiveNote.vue
+++ src/components/ActiveNote.vue
@@ -4,7 +4,7 @@
       <section class="flex-1">
         <textarea :value="activeNote.body" @input="updateNote" class="w-full h-full p-3 | bg-gray-200"></textarea>
       </section>
-      <article v-html="markdown" class="p-3 | bg-gray-900 text-white | flex-1 | mdscss"></article>
+      <ActiveNoteHTML :body="activeNote.body" class="p-3 | bg-gray-900 text-white | flex-1" />
     </div>
     <!-- Note Info and actions -->
     <section class="mt-3 | flex justify-end">
@@ -16,12 +16,16 @@
 
 <script>
 import { computed } from 'vue'
-import { marked } from 'marked'
-import DOMPurify from 'dompurify'
+
 import NoteStore from '@/stores/notes'
+import ActiveNoteHTML from './ActiveNoteHTML.vue'
 
 export default {
   name: 'ActiveNote',
+
+  components: {
+    ActiveNoteHTML,
+  },
 
   setup() {
     const noteStore = NoteStore()
@@ -39,72 +43,13 @@ export default {
       // puedo tocar el estado directamente, pero para eso hemos hecho esta función...
       noteStore.setActiveNote()
     }
-    // Para trasformar markdown a html y lo sanetizamos??? (vaya palabrajo!!!) para que el usuario no meta nada raro...
-    const markdown = computed(() => (activeNote.value ? marked(DOMPurify.sanitize(activeNote.value.body)) : null))
 
     return {
       // Podríamos devolver activeNote directamente pero luego habrá consultas remotas...
       activeNote,
       updateNote,
       closeNote,
-      markdown,
     }
   },
 }
 </script>
-
-<style scoped>
-.mdscss {
-  font-size: 1rem;
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, sans-serif;
-}
-.mdscss h1 {
-  font-size: 3.375em;
-  font-weight: bold;
-}
-.mdscss h2 {
-  font-size: 2.25em;
-  font-weight: bold;
-}
-.mdscss h3 {
-  font-size: 1.5em;
-  font-weight: bold;
-}
-.mdscss h4 {
-  font-size: 1rem;
-  font-weight: bold;
-}
-.mdscss strong {
-  font-weight: bold;
-}
-.mdscss em {
-  font-style: italic;
-}
-.mdscss small {
-  font-size: 0.667em;
-}
-.mdscss code {
-  font-family: Consolas, monaco, monospace;
-}
-.mdscss li {
-  line-height: 1.5em;
-}
-.mdscss p {
-  line-height: 1.5em;
-}
-.mdscss blockquote {
-  margin-left: 0;
-  padding-left: 0.5rem;
-  border-left: 0.5rem solid #ddd;
-}
-.mdscss pre {
-  background: #f8f8f8;
-  padding: 1em;
-}
-.mdscss * + * {
-  margin-top: 1.5rem;
-}
-.mdscss li + li {
-  margin-top: 0;
-}
-</style>
